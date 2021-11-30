00a4a0287fbc4d237246fe189a6185c9ec40b25e
Vladyslav Takhtai <vrein@tuta.io>
2021-11-30 11:48:15 +0200

    Update .git_pull.sh

--- .git_pull.sh
+++ .git_pull.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
 git pull --no-tags
-git fetch --prune origin +refs/tags/*:refs/tags/*
 git submodule init
 git submodule update -f --checkout
-git submodule foreach "git checkout master && git pull && git fetch --prune origin +refs/tags/*:refs/tags/*"
+git submodule foreach "git checkout master && git pull --no-tags"
