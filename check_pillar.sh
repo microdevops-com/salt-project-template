a59afca6c86ea6e7a69a51e3a465b39dd26e7c2f
Eugene Antonov <jetman@sysadm.ws>
2021-12-07 19:34:43 +0200

    more debug on pillar check

--- check_pillar.sh
+++ check_pillar.sh
@@ -5,4 +5,7 @@ TMP_IMAGE="$(cat /dev/urandom | tr -dc 'a-z' | fold -w 20 | head -n 1)"
 docker build --pull -t ${TMP_IMAGE} .
 docker run --rm ${TMP_IMAGE} -- /.check_pillar_for_roster.sh || GRAND_EXIT=1
 docker rmi -f ${TMP_IMAGE}
+if [[ ${GRAND_EXIT} != 0 ]]; then
+	echo ERROR: Check faile: pillar error found
+fi
 exit $GRAND_EXIT
