d21bbd67f157a7b8de0dab2730fbe7eb8137a9df
Eugene Antonov <jetman@sysadm.ws>
2021-12-08 09:09:18 +0200

    pre-push hook set in pull script

--- .git_pull.sh
+++ .git_pull.sh
@@ -4,3 +4,4 @@ git pull --no-tags
 git submodule init
 git submodule update -f --checkout
 git submodule foreach "git checkout master && git pull --no-tags"
+ln -sf ../../.githooks/pre-push .git/hooks/pre-push
