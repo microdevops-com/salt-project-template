eb3780d8dafe2f135f072d6b1384c75c41ceed8e
Eugene Antonov <jetman@sysadm.ws>
2021-12-10 20:50:14 +0200

    better docker image name

--- check_pillar.sh
+++ check_pillar.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 GRAND_EXIT=0
 
-TMP_IMAGE="$(cat /dev/urandom | tr -dc 'a-z' | fold -w 20 | head -n 1)"
-docker build --pull -t ${TMP_IMAGE} .
-docker run --rm ${TMP_IMAGE} -- /.check_pillar_for_roster.sh || GRAND_EXIT=1
-docker rmi -f ${TMP_IMAGE}
+DOCKER_IMAGE="${PWD##*/}:${USER}"
+docker build --pull -t ${DOCKER_IMAGE} .
+docker run --rm ${DOCKER_IMAGE} -- /.check_pillar_for_roster.sh || GRAND_EXIT=1
 if [[ ${GRAND_EXIT} != 0 ]]; then
 	echo ERROR: Check failed: pillar error found
 fi
