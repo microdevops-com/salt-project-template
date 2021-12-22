dc15d2ac7de930d4032929d16afb9425b8d82371
Eugene Antonov <jetman@sysadm.ws>
2021-12-22 09:17:04 +0200

    echo

--- check_pillar.sh
+++ check_pillar.sh
@@ -2,6 +2,7 @@
 GRAND_EXIT=0
 
 DOCKER_IMAGE="${PWD##*/}:${USER}"
+echo Building and running docker for pillar check
 docker build --quiet --pull -t ${DOCKER_IMAGE} .
 docker run --rm ${DOCKER_IMAGE} -- /.check_pillar_for_roster.sh || GRAND_EXIT=1
 if [[ ${GRAND_EXIT} != 0 ]]; then
