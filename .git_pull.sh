74d6ecd21acaca6af574d642ca7c3e304b57fb81
Eugene Antonov <jetman@sysadm.ws>
2022-05-26 09:42:07 +0300

    no-tags not needed anymore

--- .git_pull.sh
+++ .git_pull.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
-git pull --no-tags
+git pull
 git submodule init
 git submodule update -f --checkout
-git submodule foreach "git checkout master && git pull --no-tags"
+git submodule foreach "git checkout master && git pull"
 ln -sf ../../.githooks/pre-push .git/hooks/pre-push
