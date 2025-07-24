fe7fa9db36d130820dc10c64f43ae8b52b9511fe
Eugene Antonov <jetman@microdevops.com>
2025-07-24 20:32:49 +0300

    docker build from which dir run check

--- .docker_build.sh
+++ .docker_build.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+
+# This script is intended to be run from the root of the repository.
+if [[ "${BASH_SOURCE[0]}" != "./.docker_build.sh" ]]; then
+    echo "This script must be run from the root of the repository."
+    exit 1
+fi
+
 docker build --pull -t ${PWD##*/}:$USER .
