9fc40013a5f9e78fb9a0044c941bb884ac575163
Vladyslav Takhtai <vladyslav@vrn.red>
2025-12-02 14:19:24 +0200

    Update
    - Fix .docker_run.sh execution in docker
    - Update gtpl func

--- .docker_run.sh
+++ .docker_run.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+
+if [[ $HOSTNAME =~ docker ]]; then
+    docker run --rm -v $SSH_AUTH_SOCK:/root/.ssh-agent -e SSH_AUTH_SOCK=/root/.ssh-agent ${PWD##*/}:$USER -- "$@"
+    exit $?
+fi
+
 unset dbuild drun direxpand gtpl
 dir=${BASH_SOURCE[0]}
 dir=${dir%/*}
