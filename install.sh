5f508fdf2fde082c0c04c5c2cacbf56cb5cf8e51
Eugene Antonov <jetman@microdevops.com>
2026-09-24 17:15:15 +0200

    fix(k0s): py files path change in k0s formula

--- install.sh
+++ install.sh
@@ -241,7 +241,7 @@ add_submodule salt-k0s-formula $1/formulas https://github.com/SomeBlackMagic/sal
 
 # Copy state py files instead of symlinks as symlinks stopped working for this
 cp --remove-destination $1/formulas/salt-cloudflare/cloudflare.py $1/salt/_states
-cp --remove-destination $1/formulas/salt-k0s-formula/k0s/_states/*.py $1/salt/_states
+cp --remove-destination $1/formulas/salt-k0s-formula/_states/*.py $1/salt/_states
 
 rsync_without_delete pillar $1/pillar
 
